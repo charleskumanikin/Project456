5b5eb02cb7e8e9ef549f6aeb80ef1151fb718327
Adrian <81994521+charleskumanikin@users.noreply.github.com>
2022-02-11 18:17:52 +0200

    Add files via upload

---
+++ Project456/Project456/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project456
+//
+//  Created by Adrian Kumanikin on 11.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
